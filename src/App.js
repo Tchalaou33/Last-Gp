0036763df441956a66815d5ece8485b855bf046e
Tchalaou33 <attchalaou@gmail.com>
2021-12-20 15:30:58 -0500

    added background color to app.css, and added import dependency for react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       
     </div>
+    <h2>Netflix Clone</h2>
+    </BrowserRouter>
   );
 }
 
