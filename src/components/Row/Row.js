ac9c3326ed9eafa93c70203c1853d495c046b44f
Tchalaou33 <attchalaou@gmail.com>
2021-12-21 18:43:09 -0500

    syntax error corrected for accessing movies

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,17 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import axios from
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({title, fetchUrl, isLargeRow}) => {
     const [movies, setMovies] = useState([]);
     useEffect(() =>{
         const fetchData = async () => {
-            // const response = await axios.
-            // yarn add axios: START HERE
-        }
-    })
+            const response = await axios.get(
+                `${process.env.REACT_APP_API}${fetchUrl}`
+                );
+            setMovies(response.data.results);
+            return response
+            
+        };
+
+        fetchData();
+    }, []);
+
+    console.log("movies", movies);
     return (
         <div>
-            
+            <h2>Row</h2>
         </div>
     );
 };
