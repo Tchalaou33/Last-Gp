27775bc8fec47f1bf0528eba18afb4b3a2546202
Tchalaou33 <attchalaou@gmail.com>
2021-12-21 16:14:45 -0500

    building row component

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from
 
-const Row = () => {
+const Row = ({title, fetchUrl, isLargeRow}) => {
+    const [movies, setMovies] = useState([]);
+    useEffect(() =>{
+        const fetchData = async () => {
+            // const response = await axios.
+            // yarn add axios: START HERE
+        }
+    })
     return (
         <div>
             
         </div>
-    )
-}
+    );
+};
 
-export default Row
+export default Row;
