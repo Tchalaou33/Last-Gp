bb22935468988bbe020bcb1f8eb9358af310e2c6
Tchalaou33 <attchalaou@gmail.com>
2021-12-28 23:25:05 -0500

    made css changes to the signin button in header.css

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import api from "../../api/api";
+import "./Banner.css";
+
+
+const Banner = () => {
+    const [movie, setMovie] = useSate([])
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Banner
