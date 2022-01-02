11f1e42d0048a2b4a7f4b373f148ae55e5723a53
Tchalaou33 <attchalaou@gmail.com>
2022-01-02 17:33:19 -0500

    added signing route to app.js, created form and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
+import Signin from "./components/Signin/Signin";
 import Home from "./pages/Home";
 import NetFlixShow from "./pages/NetFlixShow";
 
 
 const App = () => {
+
   return (
     <BrowserRouter>
     
     <div className="App">
+      {' '}
+      <Signin/>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/netflix-show" element={<NetFlixShow/>} />
+       
       </Routes>
     </div>
     </BrowserRouter>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -8,10 +8,13 @@ const Signin = () => {
                 <h1>Sign In</h1>
                 <input placeholder="Name" type="name" />
                 <input placeholder="Password" type="password" />
+                <button type="submit">Sign In</button>
+                
             </form>
             
         </div>
     );
 };
+
 
 export default Signin;
